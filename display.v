13a068afcbe67dcfe60cb477b09fcf1ee08de15d
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2023-10-07 09:38:51 -0300

    feat: adicionados sianis de controle reset, clock e ready

--- display.v
+++ display.v
@@ -1,14 +1,23 @@
 module Display(
+    input wire clock,
+    input wire reset,
+    input wire ready,
     input entrada,
     output reg [6:0] saida
 );
     
-    always @* begin
-        case(entrada)
-            1'b0: saida = 7'b0111111;
-            1'b1: saida = 7'b0000110;
-            default: saida = 7'b1111111;
-        endcase
+    always @(posedge clock or posedge reset) begin
+        if (reset) begin
+            saida = 7'b0000000;
+        end
+        else if (ready) begin
+            #1;
+            case(entrada)
+                1'b0: saida = 7'b0111111;
+                1'b1: saida = 7'b0000110;
+                default: saida = 7'b1111111;
+            endcase
+        end
     end
 
 endmodule
