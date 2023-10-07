995dc5e78b0c8943f07e12b3e2b8eedb567493fc
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2023-10-07 09:41:18 -0300

    feat: atualizado testbench para utilizar o modulo main

--- testbench.v
+++ testbench.v
@@ -17,24 +17,17 @@ wire[6:0] display1;
 
 wire[6:0] display0;
 
-Codificador uut (
+Main uut (
     .clock(clock),
     .reset(reset),
     .ready(ready),
-    .A(entrada[3]),
-    .B(entrada[2]),
-    .C(entrada[1]),
-    .D(entrada[0]),
-    .S3(saida[3]),
-    .S2(saida[2]),
-    .S1(saida[1]),
-    .S0(saida[0])
+    .entrada(entrada),
+    .saida(saida),
+    .display3(display3),
+    .display2(display2),
+    .display1(display1),
+    .display0(display0)
 );
-
-Display display3_uut (.entrada(saida[3]), .saida(display3));
-Display display2_uut (.entrada(saida[2]), .saida(display2));
-Display display1_uut (.entrada(saida[1]), .saida(display1));
-Display display0_uut (.entrada(saida[0]), .saida(display0));
 
 
 always begin
@@ -44,8 +37,9 @@ end
 initial begin
     $dumpfile("testbench.vcd");
     $dumpvars(0, testbench);
+    $display("Main:");
     $display("A B C D | S3 S2 S1 S0 |> Number3 Number2 Number1 Number0");
-    $display("---------------------------");
+    $display("--------------------------------------------------------");
     
     clock = 0;
     reset = 1;
@@ -60,7 +54,9 @@ initial begin
         ready = 0;
         #10;
 
-       $display("%b %b %b %b | %b  %b  %b  %b  |> %b %b %b %b", entrada[3], entrada[2], entrada[1], entrada[0], saida[3], saida[2], saida[1], saida[0], display3, display2, display1, display0);
+       $display("%b %b %b %b | %b  %b  %b  %b  |> %b %b %b %b", 
+       entrada[3], entrada[2], entrada[1], entrada[0], saida[3], 
+       saida[2], saida[1], saida[0], display3, display2, display1, display0);
 
         contador = contador + 1;
     end
